7aba753ca062bfed06fe296fb7e466003c74b4fa
LOuroboros <lunosouroboros@gmail.com>
2022-04-21 06:44:47 -0300

    Added a cue to identify a roamer's location Inspired by nonchip.

--- include/event_scripts.h
+++ include/event_scripts.h
@@ -615,5 +615,6 @@ extern const u8 EventScript_TradeCenter_Chair0[];
 extern const u8 EventScript_ConfirmLeaveCableClubRoom[];
 extern const u8 EventScript_TerminateLink[];
 extern const u8 EventScript_ThereIsNoPokemon[];
+extern const u8 EventScript_RoamerCue[];
 
 #endif // GUARD_EVENT_SCRIPTS_H
