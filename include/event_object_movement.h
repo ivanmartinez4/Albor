361a358b3cb337cf54ca15d3889f0796f62abe0f
LOuroboros <lunosouroboros@gmail.com>
2022-05-31 12:48:06 -0300

    Removed pointless declaration of an unused function Added by Ghoulslash's overworld expansion.

--- include/event_object_movement.h
+++ include/event_object_movement.h
@@ -96,7 +96,6 @@ void SetSpritePosToOffsetMapCoords(s16 *, s16 *, s16, s16);
 void ObjectEventClearHeldMovement(struct ObjectEvent *);
 void ObjectEventClearHeldMovementIfActive(struct ObjectEvent *);
 void TrySpawnObjectEvents(s16, s16);
-u8 CreateObjectSprite(u16 graphicsId, u8 a1, s16 x, s16 y, u8 z, u8 direction);
 u8 CreateObjectGraphicsSprite(u16, void (*)(struct Sprite *), s16 x, s16 y, u8 subpriority);
 u8 TrySpawnObjectEvent(u8, u8, u8);
 u8 SpawnSpecialObjectEventParameterized(u16 graphicsId, u8 movementBehavior, u8 localId, s16 x, s16 y, u8 z);
