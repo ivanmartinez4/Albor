ivanmartinez4/Albor

commit 50470d30513376e28b8d604f19845905616b9201

Made BoxPokemon track the date in which a Pokémon was generated Thanks to Jasper/Bassoonian.
Misc:
-Removed some never used ribbons because I don't need them.
-Modified CreateEgg, because having every Egg generated as if it was obtained from Lavaridge's Hot Springs sucks.
-Made RenderPlayerParty take Eggs into account.
Date of creation: Tue Oct 26 11:31:08 2021 -0300

src/pokemon_summary_screen.c
@@ -165,6 +165,9 @@ static EWRAM_DATA struct PokemonSummaryScreenData
         u8 sanity; // 0x35
         u8 OTName[17]; // 0x36
         u32 OTID; // 0x48
+        u8 dayMet;
+        u8 monthMet;
+        u8 yearMet;
     } summary;
     u16 bgTilemapBuffers[PSS_PAGE_COUNT][2][0x400];
     u8 mode;
@@ -261,6 +264,7 @@ static void BufferMonTrainerMemo(void);
 static void PrintMonTrainerMemo(void);
 static void BufferNatureString(void);
 static void GetMetLevelString(u8 *a);
+static void GetDateMetString(u8 *a, u8 *b);
 static bool8 DoesMonOTMatchOwner(void);
 static bool8 DidMonComeFromGBAGames(void);
 static bool8 IsInGamePartnerMon(void);
@@ -1527,6 +1531,9 @@ static bool8 ExtractMonDataToSummaryStruct(struct Pokemon *mon)
         sum->metLevel = GetMonData(mon, MON_DATA_MET_LEVEL);
         sum->metGame = GetMonData(mon, MON_DATA_MET_GAME);
         sum->friendship = GetMonData(mon, MON_DATA_FRIENDSHIP);
+        sum->dayMet = GetMonData(mon, MON_DATA_DAY_MET);
+        sum->monthMet = GetMonData(mon, MON_DATA_MONTH_MET);
+        sum->yearMet = GetMonData(mon, MON_DATA_YEAR_MET);
         break;
     default:
         sum->ribbonCount = GetMonData(mon, MON_DATA_RIBBON_COUNT);
@@ -3147,6 +3154,34 @@ static void PrintMonAbilityDescription(void)
     PrintTextOnWindow(AddWindowFromTemplateList(sPageInfoTemplate, PSS_DATA_WINDOW_INFO_ABILITY), gAbilityDescriptionPointers[ability], 0, 17, 0, 0);
 }
 
+static void GetDateMetString(u8 *output, u8 *output2)
+{
+    u8 data[2];
+    static const u8 *const monthNames[12]={
+        gText_Month_January,
+        gText_Month_February,
+        gText_Month_March,
+        gText_Month_April,
+        gText_Month_May,
+        gText_Month_June,
+        gText_Month_July,
+        gText_Month_August,
+        gText_Month_September,
+        gText_Month_October,
+        gText_Month_November,
+        gText_Month_December,
+    };
+    DynamicPlaceholderTextUtil_SetPlaceholderPtr(5, monthNames[sMonSummaryScreen->summary.monthMet - 1]);
+
+    data[0] = sMonSummaryScreen->summary.dayMet;
+    ConvertIntToDecimalStringN(output, data[0], STR_CONV_MODE_LEFT_ALIGN, 6);
+    DynamicPlaceholderTextUtil_SetPlaceholderPtr(6, output);
+
+    data[1] = sMonSummaryScreen->summary.yearMet;
+    ConvertIntToDecimalStringN(output2, data[1], STR_CONV_MODE_LEFT_ALIGN, 7);
+    DynamicPlaceholderTextUtil_SetPlaceholderPtr(7, output2);
+}
+
 static void BufferMonTrainerMemo(void)
 {
     struct PokeSummary *sum = &sMonSummaryScreen->summary;
@@ -3165,7 +3200,10 @@ static void BufferMonTrainerMemo(void)
     {
         u8 *metLevelString = Alloc(32);
         u8 *metLocationString = Alloc(32);
+        u8 *metDateString = Alloc(32);
+        u8 *metDateString2 = Alloc(32);
         GetMetLevelString(metLevelString);
+        GetDateMetString(metDateString, metDateString2);
 
         if (sum->metLocation < MAPSEC_NONE)
         {
@@ -3196,6 +3234,8 @@ static void BufferMonTrainerMemo(void)
         DynamicPlaceholderTextUtil_ExpandPlaceholders(gStringVar7, text);
         Free(metLevelString);
         Free(metLocationString);
+        Free(metDateString);
+        Free(metDateString2);
     }
 }
 
@@ -3208,7 +3248,6 @@ static void BufferNatureString(void)
 {
     struct PokemonSummaryScreenData *sumStruct = sMonSummaryScreen;
     DynamicPlaceholderTextUtil_SetPlaceholderPtr(2, gNatureNamePointers[sumStruct->summary.nature]);
-    DynamicPlaceholderTextUtil_SetPlaceholderPtr(5, gText_EmptyString5);
 }
 
 static void GetMetLevelString(u8 *output)
@@ -3313,6 +3352,10 @@ static void PrintEggMemo(void)
     const u8 *text;
     struct PokeSummary *sum = &sMonSummaryScreen->summary;
 
+    DynamicPlaceholderTextUtil_Reset();
+    DynamicPlaceholderTextUtil_SetPlaceholderPtr(0, sMemoNatureTextColor);
+    DynamicPlaceholderTextUtil_SetPlaceholderPtr(1, sMemoMiscTextColor);
+
     if (sMonSummaryScreen->summary.sanity != 1)
     {
         if (sum->metLocation == METLOC_FATEFUL_ENCOUNTER)
@@ -3322,7 +3365,20 @@ static void PrintEggMemo(void)
         else if (sum->metLocation == METLOC_SPECIAL_EGG)
             text = (DidMonComeFromRSE() == TRUE) ? gText_EggFromHotSprings : gText_EggFromTraveler;
         else
-            text = gText_OddEggFoundByCouple;
+        {
+            u8 *metLocationString = Alloc(32);
+            u8 *metDateString = Alloc(32);
+            u8 *metDateString2 = Alloc(32);
+            GetDateMetString(metDateString, metDateString2);
+
+            GetMapNameHandleAquaHideout(metLocationString, sum->metLocation);
+            DynamicPlaceholderTextUtil_SetPlaceholderPtr(4, metLocationString);
+            DynamicPlaceholderTextUtil_ExpandPlaceholders(gStringVar7, gText_EggFoundIn);
+            text = gStringVar7;
+            Free(metLocationString);
+            Free(metDateString);
+            Free(metDateString2);
+        }
     }
     else
     {

include/constants/pokemon.h
@@ -128,7 +128,7 @@
 #define MON_DATA_OT_NAME            7
 #define MON_DATA_MARKINGS           8
 #define MON_DATA_CHECKSUM           9
-#define MON_DATA_ENCRYPT_SEPARATOR 10
+#define MON_DATA_YEAR_MET          10
 #define MON_DATA_SPECIES           11
 #define MON_DATA_HELD_ITEM         12
 #define MON_DATA_MOVE1             13
@@ -190,22 +190,21 @@
 #define MON_DATA_VICTORY_RIBBON    69
 #define MON_DATA_ARTIST_RIBBON     70
 #define MON_DATA_EFFORT_RIBBON     71
-#define MON_DATA_MARINE_RIBBON     72
-#define MON_DATA_LAND_RIBBON       73
-#define MON_DATA_SKY_RIBBON        74
-#define MON_DATA_COUNTRY_RIBBON    75
-#define MON_DATA_NATIONAL_RIBBON   76
-#define MON_DATA_EARTH_RIBBON      77
-#define MON_DATA_WORLD_RIBBON      78
-#define MON_DATA_EVENT_LEGAL       79
-#define MON_DATA_KNOWN_MOVES       80
-#define MON_DATA_RIBBON_COUNT      81
-#define MON_DATA_RIBBONS           82
-#define MON_DATA_ATK2              83
-#define MON_DATA_DEF2              84
-#define MON_DATA_SPEED2            85
-#define MON_DATA_SPATK2            86
-#define MON_DATA_SPDEF2            87
+#define MON_DATA_COUNTRY_RIBBON    72
+#define MON_DATA_NATIONAL_RIBBON   73
+#define MON_DATA_EARTH_RIBBON      74
+#define MON_DATA_WORLD_RIBBON      75
+#define MON_DATA_EVENT_LEGAL       76
+#define MON_DATA_KNOWN_MOVES       77
+#define MON_DATA_RIBBON_COUNT      78
+#define MON_DATA_RIBBONS           79
+#define MON_DATA_ATK2              80
+#define MON_DATA_DEF2              81
+#define MON_DATA_SPEED2            82
+#define MON_DATA_SPATK2            83
+#define MON_DATA_SPDEF2            84
+#define MON_DATA_DAY_MET           85
+#define MON_DATA_MONTH_MET         86
 
 // Ribbon IDs used by TV and Pokénav
 #define CHAMPION_RIBBON       0
@@ -233,19 +232,16 @@
 #define VICTORY_RIBBON       22
 #define ARTIST_RIBBON        23
 #define EFFORT_RIBBON        24
-#define MARINE_RIBBON        25
-#define LAND_RIBBON          26
-#define SKY_RIBBON           27
-#define COUNTRY_RIBBON       28
-#define NATIONAL_RIBBON      29
-#define EARTH_RIBBON         30
-#define WORLD_RIBBON         31
+#define COUNTRY_RIBBON       25
+#define NATIONAL_RIBBON      26
+#define EARTH_RIBBON         27
+#define WORLD_RIBBON         28
 
-#define FIRST_GIFT_RIBBON MARINE_RIBBON
+#define FIRST_GIFT_RIBBON COUNTRY_RIBBON
 #define LAST_GIFT_RIBBON  WORLD_RIBBON
 #define NUM_GIFT_RIBBONS  (1 + LAST_GIFT_RIBBON - FIRST_GIFT_RIBBON)
 
-// The above gift ribbons (Marine - World) are
+// The above gift ribbons (Country - World) are
 // special distribution ribbons that correspond to
 // 1 bit each in the Pokémon struct. Gen 4 hard-codes
 // each of these to the given name. In Gen 3 they're

include/pokemon.h
@@ -85,15 +85,17 @@ struct BoxPokemon
     u32 victoryRibbon:1;
     u32 artistRibbon:1;
     u32 effortRibbon:1;
-    u32 marineRibbon:1; // never distributed
-    u32 landRibbon:1; // never distributed
-    u32 skyRibbon:1; // never distributed
     u32 countryRibbon:1; // distributed during Pokémon Festa '04 and '05 to tournament winners
     u32 nationalRibbon:1;
     u32 earthRibbon:1;
     u32 worldRibbon:1; // distributed during Pokémon Festa '04 and '05 to tournament winners
     u32 eventLegal:1;
     u32 markings:4;
+
+    // Word 17: Date Met
+    u32 dayMet:5;
+    u32 monthMet:4;
+    u32 yearMet:7;
 };
 
 struct Pokemon

include/strings.h
@@ -982,6 +982,7 @@ extern const u8 gText_PeculiarEggTrade[];
 extern const u8 gText_EggFromTraveler[];
 extern const u8 gText_EggFromHotSprings[];
 extern const u8 gText_OddEggFoundByCouple[];
+extern const u8 gText_EggFoundIn[];
 extern const u8 gText_None[];
 extern const u8 gText_RibbonsVar1[];
 extern const u8 gText_OneDash[];
@@ -3122,5 +3123,19 @@ extern const u8 gText_SavingDate[];
 // Route 118 event
 extern const u8 gText_Badge[];
 extern const u8 gText_Badges[];
+
+// Date Met
+extern const u8 gText_Month_January[];
+extern const u8 gText_Month_February[];
+extern const u8 gText_Month_March[];
+extern const u8 gText_Month_April[];
+extern const u8 gText_Month_May[];
+extern const u8 gText_Month_June[];
+extern const u8 gText_Month_July[];
+extern const u8 gText_Month_August[];
+extern const u8 gText_Month_September[];
+extern const u8 gText_Month_October[];
+extern const u8 gText_Month_November[];
+extern const u8 gText_Month_December[];
 
 #endif // GUARD_STRINGS_H

src/evolution_scene.c
@@ -577,6 +577,8 @@ static void CreateShedinja(u16 preEvoSpecies, struct Pokemon* mon)
         RemoveBagItem(ball, 1);
         #endif
 
+        SetMonData(&gPlayerParty[gPlayerPartyCount], MON_DATA_YEAR_MET, &data);
+
         for (i = MON_DATA_COOL_RIBBON; i < MON_DATA_COOL_RIBBON + CONTEST_CATEGORIES_COUNT; i++)
             SetMonData(&gPlayerParty[gPlayerPartyCount], i, &data);
 

src/pokemon.c
@@ -3236,6 +3236,7 @@ void CreateBoxMon(struct BoxPokemon *boxMon, u16 species, u8 level, u8 fixedIV, 
     u32 shinyValue;
     u8 maxIV = MAX_IV_MASK;
     u8 statIDs[] = {0, 1, 2, 3, 4, 5};
+    struct SiiRtcInfo rtc;
 
     ZeroBoxMonData(boxMon);
 
@@ -3376,6 +3377,15 @@ void CreateBoxMon(struct BoxPokemon *boxMon, u16 species, u8 level, u8 fixedIV, 
         value = personality & 1;
         SetBoxMonData(boxMon, MON_DATA_ABILITY_NUM, &value);
     }
+
+    // Date Met
+    RtcGetDateTime(&rtc);
+    value = ConvertBcdToBinary(rtc.day);
+    SetBoxMonData(boxMon, MON_DATA_DAY_MET, &value);
+    value = ConvertBcdToBinary(rtc.month);
+    SetBoxMonData(boxMon, MON_DATA_MONTH_MET, &value);
+    value = ConvertBcdToBinary(rtc.year);
+    SetBoxMonData(boxMon, MON_DATA_YEAR_MET, &value);
 
     GiveBoxMonInitialMoveset(boxMon);
 }
@@ -4398,6 +4408,15 @@ u32 GetBoxMonData(struct BoxPokemon *boxMon, s32 field, u8 *data)
     case MON_DATA_MARKINGS:
         retVal = boxMon->markings;
         break;
+    case MON_DATA_YEAR_MET:
+        retVal = boxMon->yearMet;
+        break;
+    case MON_DATA_DAY_MET:
+        retVal = boxMon->dayMet;
+        break;
+    case MON_DATA_MONTH_MET:
+        retVal = boxMon->monthMet;
+        break;
     case MON_DATA_SPECIES:
         retVal = boxMon->species;
         break;
@@ -4533,15 +4552,6 @@ u32 GetBoxMonData(struct BoxPokemon *boxMon, s32 field, u8 *data)
     case MON_DATA_EFFORT_RIBBON:
         retVal = boxMon->effortRibbon;
         break;
-    case MON_DATA_MARINE_RIBBON:
-        retVal = boxMon->marineRibbon;
-        break;
-    case MON_DATA_LAND_RIBBON:
-        retVal = boxMon->landRibbon;
-        break;
-    case MON_DATA_SKY_RIBBON:
-        retVal = boxMon->skyRibbon;
-        break;
     case MON_DATA_COUNTRY_RIBBON:
         retVal = boxMon->countryRibbon;
         break;
@@ -4597,9 +4607,6 @@ u32 GetBoxMonData(struct BoxPokemon *boxMon, s32 field, u8 *data)
             retVal += boxMon->victoryRibbon;
             retVal += boxMon->artistRibbon;
             retVal += boxMon->effortRibbon;
-            retVal += boxMon->marineRibbon;
-            retVal += boxMon->landRibbon;
-            retVal += boxMon->skyRibbon;
             retVal += boxMon->countryRibbon;
             retVal += boxMon->nationalRibbon;
             retVal += boxMon->earthRibbon;
@@ -4620,9 +4627,6 @@ u32 GetBoxMonData(struct BoxPokemon *boxMon, s32 field, u8 *data)
                 | (boxMon->victoryRibbon << 17)
                 | (boxMon->artistRibbon << 18)
                 | (boxMon->effortRibbon << 19)
-                | (boxMon->marineRibbon << 20)
-                | (boxMon->landRibbon << 21)
-                | (boxMon->skyRibbon << 22)
                 | (boxMon->countryRibbon << 23)
                 | (boxMon->nationalRibbon << 24)
                 | (boxMon->earthRibbon << 25)
@@ -4724,6 +4728,15 @@ void SetBoxMonData(struct BoxPokemon *boxMon, s32 field, const void *dataArg)
     }
     case MON_DATA_MARKINGS:
         SET8(boxMon->markings);
+        break;
+    case MON_DATA_YEAR_MET:
+        SET32(boxMon->yearMet);
+        break;
+    case MON_DATA_MONTH_MET:
+        SET32(boxMon->monthMet);
+        break;
+    case MON_DATA_DAY_MET:
+        SET32(boxMon->dayMet);
         break;
     case MON_DATA_SPECIES:
     {
@@ -4867,15 +4880,6 @@ void SetBoxMonData(struct BoxPokemon *boxMon, s32 field, const void *dataArg)
         break;
     case MON_DATA_EFFORT_RIBBON:
         SET8(boxMon->effortRibbon);
-        break;
-    case MON_DATA_MARINE_RIBBON:
-        SET8(boxMon->marineRibbon);
-        break;
-    case MON_DATA_LAND_RIBBON:
-        SET8(boxMon->landRibbon);
-        break;
-    case MON_DATA_SKY_RIBBON:
-        SET8(boxMon->skyRibbon);
         break;
     case MON_DATA_COUNTRY_RIBBON:
         SET8(boxMon->countryRibbon);

src/pokemon_size_record.c
@@ -38,7 +38,6 @@ static const struct UnknownStruct sBigMonSizeTable[] =
 
 static const u8 sGiftRibbonsMonDataIds[GIFT_RIBBONS_COUNT] =
 {
-    MON_DATA_MARINE_RIBBON, MON_DATA_LAND_RIBBON, MON_DATA_SKY_RIBBON,
     MON_DATA_COUNTRY_RIBBON, MON_DATA_NATIONAL_RIBBON, MON_DATA_EARTH_RIBBON,
     MON_DATA_WORLD_RIBBON
 };

src/pokenav_ribbons_summary.c
@@ -131,9 +131,6 @@ struct
     {1, 1, VICTORY_RIBBON,       FALSE},
     {1, 1, ARTIST_RIBBON,        FALSE},
     {1, 1, EFFORT_RIBBON,        FALSE},
-    {1, 1, MARINE_RIBBON,        TRUE},
-    {1, 1, LAND_RIBBON,          TRUE},
-    {1, 1, SKY_RIBBON,           TRUE},
     {1, 1, COUNTRY_RIBBON,       TRUE},
     {1, 1, NATIONAL_RIBBON,      TRUE},
     {1, 1, EARTH_RIBBON,         TRUE},
@@ -1112,9 +1109,6 @@ struct
     [VICTORY_RIBBON]       = { RIBBONGFX_VICTORY,        TO_PAL_OFFSET(PALTAG_RIBBON_ICONS_1)},
     [ARTIST_RIBBON]        = { RIBBONGFX_ARTIST,         TO_PAL_OFFSET(PALTAG_RIBBON_ICONS_2)},
     [EFFORT_RIBBON]        = { RIBBONGFX_EFFORT,         TO_PAL_OFFSET(PALTAG_RIBBON_ICONS_3)},
-    [MARINE_RIBBON]        = { RIBBONGFX_GIFT_1,         TO_PAL_OFFSET(PALTAG_RIBBON_ICONS_2)},
-    [LAND_RIBBON]          = { RIBBONGFX_GIFT_1,         TO_PAL_OFFSET(PALTAG_RIBBON_ICONS_4)},
-    [SKY_RIBBON]           = { RIBBONGFX_GIFT_1,         TO_PAL_OFFSET(PALTAG_RIBBON_ICONS_5)},
     [COUNTRY_RIBBON]       = { RIBBONGFX_GIFT_2,         TO_PAL_OFFSET(PALTAG_RIBBON_ICONS_4)},
     [NATIONAL_RIBBON]      = { RIBBONGFX_GIFT_2,         TO_PAL_OFFSET(PALTAG_RIBBON_ICONS_5)},
     [EARTH_RIBBON]         = { RIBBONGFX_GIFT_3,         TO_PAL_OFFSET(PALTAG_RIBBON_ICONS_1)},

commit 0841fdd092be54b3a8715727cdbd4f502276442f

Fixed issue introduced in 50470d30513376e28b8d604f19845905616b9201 The game was freezing when the game prints "Take good care of [whatever]" as the trade finished. Thanks to MGriffin, we found the reason why; the ribbon changes made in the commit linked in the title. There's a function that seems to check if a Pokémon that is received during a trade has a special ribbon. A ribbon for being in the party that defeated the Pokémon League, a ribbon awarded for completing 50 smth battles in the Battle Tower and such. This function has a hardcoded check, and that was the root of the problem.

src/pokemon_summary_screen.c
@@ -166,9 +166,9 @@ static EWRAM_DATA struct PokemonSummaryScreenData
         u8 sanity; // 0x35
         u8 OTName[17]; // 0x36
         u32 OTID; // 0x48
-        u8 dayMet;
-        u8 monthMet;
-        u8 yearMet;
+        u32 dayMet; // 0x4C
+        u32 monthMet; // 0x50
+        u32 yearMet; // 0x54
     } summary;
     u16 bgTilemapBuffers[PSS_PAGE_COUNT][2][0x400];
     u8 mode;

src/trade.c
@@ -50,6 +50,7 @@
 #include "constants/rgb.h"
 #include "constants/songs.h"
 #include "constants/union_room.h"
+#include "rtc.h"
 
 // The following tags are offsets from GFXTAG_MENU_TEXT
 // They're looped over in CB2_CreateTradeMenu and CB2_ReturnToTradeMenu
@@ -4450,6 +4451,8 @@ static void _CreateInGameTradePokemon(u8 whichPlayerMon, u8 whichInGameTrade)
     u8 metLocation = METLOC_IN_GAME_TRADE;
     u8 isMail;
     struct Pokemon *pokemon = &gEnemyParty[0];
+    struct SiiRtcInfo rtc;
+    u16 value;
 
     CreateMon(pokemon, inGameTrade->species, level, USE_RANDOM_IVS, TRUE, inGameTrade->personality, OT_ID_PRESET, inGameTrade->otId);
 
@@ -4485,6 +4488,16 @@ static void _CreateInGameTradePokemon(u8 whichPlayerMon, u8 whichInGameTrade)
             SetMonData(pokemon, MON_DATA_HELD_ITEM, &inGameTrade->heldItem);
         }
     }
+
+    // Date Met
+    RtcGetDateTime(&rtc);
+    value = ConvertBcdToBinary(rtc.day);
+    SetMonData(pokemon, MON_DATA_DAY_MET, &value);
+    value = ConvertBcdToBinary(rtc.month);
+    SetMonData(pokemon, MON_DATA_MONTH_MET, &value);
+    value = ConvertBcdToBinary(rtc.year);
+    SetMonData(pokemon, MON_DATA_YEAR_MET, &value);
+
     CalculateMonStats(&gEnemyParty[0]);
 }
 
@@ -4771,7 +4784,7 @@ static void CheckPartnersMonForRibbons(void)
 {
     u8 i;
     u8 numRibbons = 0;
-    for (i = 0; i < 12; i ++)
+    for (i = 0; i < (MON_DATA_EFFORT_RIBBON - MON_DATA_CHAMPION_RIBBON); i ++)
     {
         numRibbons += GetMonData(&gEnemyParty[gSelectedTradeMonPositions[TRADE_PARTNER] % PARTY_SIZE], MON_DATA_CHAMPION_RIBBON + i);
     }

src/wonder_trade.c
@@ -20,6 +20,7 @@
 #include "field_weather.h"
 #include "constants/weather.h"
 #endif
+#include "rtc.h"
 
 extern struct Evolution gEvolutionTable[][EVOS_PER_MON];
 
@@ -188,6 +189,8 @@ void CreateWonderTradePokemon(u8 whichPlayerMon)
             _("ERROR"), FEMALE, playerSpecies
         }
     };
+    struct SiiRtcInfo rtc;
+    u16 value;
 
     genderOT = getWonderTradeOT(nameOT);
     inGameTrade = &gIngameTrades[0];
@@ -270,6 +273,16 @@ void CreateWonderTradePokemon(u8 whichPlayerMon)
     SetMonData(pokemon, MON_DATA_TOUGH, &inGameTrade->stats[4]);
     SetMonData(pokemon, MON_DATA_MET_LOCATION, &metLocation);
     SetMonData(pokemon, MON_DATA_OT_GENDER, &genderOT);
+
+    // Date Met
+    RtcGetDateTime(&rtc);
+    value = ConvertBcdToBinary(rtc.day);
+    SetMonData(pokemon, MON_DATA_DAY_MET, &value);
+    value = ConvertBcdToBinary(rtc.month);
+    SetMonData(pokemon, MON_DATA_MONTH_MET, &value);
+    value = ConvertBcdToBinary(rtc.year);
+    SetMonData(pokemon, MON_DATA_YEAR_MET, &value);
+
     CalculateMonStats(&gEnemyParty[0]);
 }
 