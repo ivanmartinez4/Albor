5be30b8ecb3f041119b86ce88c9eca0b7d840dd9
ivanmartinezl <ivan.martinezl@icloud.com>
2022-08-15 16:05:46 +0200

    Last ball/Pokedex palette/Wild encounters

--- src/pokedex.c
+++ src/pokedex.c
@@ -407,7 +407,7 @@ static void CreateTypeIconSprites(void);
 //Stats screen HGSS_Ui
 #define SCROLLING_MON_X 146
 #define HGSS_DECAPPED 1 //0 false, 1 true
-#define HGSS_DARK_MODE 1 //0 false, 1 true
+#define HGSS_DARK_MODE 0 //0 false, 1 true
 #define HGSS_HIDE_UNSEEN_EVOLUTION_NAMES 0 //0 false, 1 true
 static u16 NationalPokedexNumToSpeciesHGSS(u16 nationalNum);
 static void LoadTilesetTilemapHGSS(u8 page);
