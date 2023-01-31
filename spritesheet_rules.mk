8eb77a4546a72ce071724bb5b126074bf8870435
ivanmartinez4 <ivangithub@hotmail.com>
2023-02-01 00:25:15 +0100

    Vulpix Alola

--- spritesheet_rules.mk
+++ spritesheet_rules.mk
@@ -819,6 +819,9 @@ $(OBJEVENTGFXDIR)/pokemon/raichu.4bpp: %.4bpp: %.png
 $(OBJEVENTGFXDIR)/pokemon/alolan_raichu.4bpp: %.4bpp: %.png
 	$(GFX) $< $@ -mwidth 4 -mheight 4
 
+$(OBJEVENTGFXDIR)/pokemon/alolan_vulpix.4bpp: %.4bpp: %.png
+	$(GFX) $< $@ -mwidth 4 -mheight 4
+
 $(OBJEVENTGFXDIR)/pokemon/sandshrew.4bpp: %.4bpp: %.png
 	$(GFX) $< $@ -mwidth 4 -mheight 4
 
--- src/data/object_events/object_event_pic_tables.h
+++ src/data/object_events/object_event_pic_tables.h
@@ -2208,6 +2208,14 @@ static const struct SpriteFrameImage sPicTable_AlolanRaichu[] = {
     overworld_frame(gObjectEventPic_AlolanRaichu, 4, 4, 4),
     overworld_frame(gObjectEventPic_AlolanRaichu, 4, 4, 5),
 };
+static const struct SpriteFrameImage sPicTable_AlolanVulpix[] = {
+    overworld_frame(gObjectEventPic_AlolanVulpix, 4, 4, 0),
+    overworld_frame(gObjectEventPic_AlolanVulpix, 4, 4, 1),
+    overworld_frame(gObjectEventPic_AlolanVulpix, 4, 4, 2),
+    overworld_frame(gObjectEventPic_AlolanVulpix, 4, 4, 3),
+    overworld_frame(gObjectEventPic_AlolanVulpix, 4, 4, 4),
+    overworld_frame(gObjectEventPic_AlolanVulpix, 4, 4, 5),
+};
 static const struct SpriteFrameImage sPicTable_Sandshrew[] = {
     overworld_frame(gObjectEventPic_Sandshrew, 4, 4, 0),
     overworld_frame(gObjectEventPic_Sandshrew, 4, 4, 1),
